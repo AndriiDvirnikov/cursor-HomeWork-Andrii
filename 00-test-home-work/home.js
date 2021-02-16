c40a934586b707a5076bbe0db2e997434105f265
AndriiDvornikov <rostvorgame@gmail.com>
2021-02-16 08:07:25 +0200

    Last update first hw

--- 00-test-home-work/home.js
+++ 00-test-home-work/home.js
@@ -1,8 +1,8 @@
-let price1 = 15.678;
-let price2 = 123.965;
-let price3 = 90.2345;
+const price1 = 15.678;
+const price2 = 123.965;
+const price3 = 90.2345;
 
-let money = 500;
+const money = 500;
 
 /* МАКСИМАЛЬНА ЦІНА */
 console.log("Максимальна ціна = " +  Math.max(price1,price2,price3)+";");
@@ -11,11 +11,11 @@ console.log("Максимальна ціна = " +  Math.max(price1,price2,price
 console.log("Мінімальна ціна = " +  Math.min(price1,price2,price3)+";");
 
 /* СУМА ЦІН */
-let sum_price = price1 + price2 + price3;
+const sum_price = price1 + price2 + price3;
 console.log("Сума цін = " + sum_price + ";");
 
 /* СУМА БЕЗ КОПІЙОК */
-let sum_trunk = Math.trunc(price1) + Math.trunc(price2) + Math.trunc(price3);
+const sum_trunk = Math.trunc(price1) + Math.trunc(price2) + Math.trunc(price3);
 console.log("Сума без копійок = " + sum_trunk + ";");
 
 /* ОКРУГЛЕННЯ ДО БІЛЬШОГО Math.floor */
@@ -37,21 +37,23 @@ if ((Math.floor(sum_price)%2)===0){
     }
 */
 /* РЕШТА */
-let rest = 500 - sum_price;
+const rest = 500 - sum_price;
 console.log("Решта = " + rest);
 
 /* СЕРЕДНЄ ЗНАЧЕННЯ */
-let avarege = (price1 + price2 + price3)/2;
+const avarege = (price1 + price2 + price3)/2;
 console.log("Средня ціна = "  +avarege);
 console.log("Средня ціна округлена до другого знаку після коми = " + (Math.round(avarege*100))/100);
 
 /* ЗНИЖКА */
-let rNum = Math.random();
+const rNum = Math.random();
 console.log("Знижка = " + Math.round(rNum*100) + " %");
 let sale = (Math.round(rNum*100)/100)*sum_price;
 sale = (Math.round(sale*100))/100;
 console.log("Знижка в грн = " + sale);
 
 /* ЧИСТИЙ ПРИБУТОК ПІСЛЯ ЗНИЖКИ */
-let profit = (sum_price/2)-sale;
+const profit = (sum_price/2)-sale;
 console.log("Чистий прибуток = " + Math.round( profit));
+
+
