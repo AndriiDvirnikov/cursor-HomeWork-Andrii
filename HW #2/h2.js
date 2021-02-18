6aeb025a2f4fb12a1dd4339f0eccbc61d895ab1c
AndriiDvornikov <rostvorgame@gmail.com>
2021-02-18 12:55:29 +0200

    HW#2 start

---
+++ HW #2/h2.js
@@ -0,0 +1,15 @@
+let n;
+do{
+  n = prompt("Введіть число N",);
+    n = parseInt(n,10);
+} while(isNaN(n));
+console.log (n, typeof(n));
+
+let m;
+do{
+  m = prompt("Введіть число M",);
+    m = parseInt(m,10);
+} while(isNaN(m));
+console.log (m,typeof(m));
+
+
