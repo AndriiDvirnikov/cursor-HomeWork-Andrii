152dfa038b66d0e2df930aed0901913f9c60ec1d
AndriiDvornikov <rostvorgame@gmail.com>
2021-03-04 09:14:45 +0200

    Uppdate HW#4

--- HW #4/h4.js
+++ HW #4/h4.js
@@ -2,6 +2,8 @@
 const students = ["Олександр", "Іван", "Олена", "Іра", "Олексій", "Світлана"];
 const themes = ["Диференційне рівняння", "Теорія автоматів", "Алгоритми і структури даних"];
 const marks = [4, 5, 5, 3, 4, 5];
+const maxmark = 5;
+const minmark = 1;
 console.log('students',students);
 console.log('themes',themes);
 console.log('marks',marks);
@@ -12,9 +14,7 @@ function printStudents (students, themes, marks){
     const arrBoy = [];
     const arrGirl = [];
     const studentCoupl = [];
-    const studentsCouplThemes = [];
-    const markStedents = [];
-    const newMark = []
+   
     
     for (let i = 0; i < students.length;i++){
         const newArr = students[i].slice(-1);
@@ -29,29 +29,69 @@ function printStudents (students, themes, marks){
     console.log('arrGirl',arrGirl);
     
     for (let i =0; i<arrBoy.length;i++){
-        newMark[i]=randomMark();
+    
         
-        studentCoupl.push([arrBoy[i],arrGirl[i],themes[i],newMark[i]]);
+        studentCoupl.push([arrBoy[i],arrGirl[i]]);
     }
     console.log(`studentCoupl`,studentCoupl)
     
-  for (let i = 0 ; i < students.length;i++){
+  
+    
+    return studentCoupl;
+}
+
+const studentCoupl = printStudents (students,themes,marks);
+
+function getStudentThemes (studentCoupl,themes){
+    const studentsCouplThemes = [];
+    for (let i =0; i<studentCoupl.length; i++){
+        const pair1 = studentCoupl[i];
+        const theme = themes[i];
+        studentsCouplThemes.push([pair1.join(' i '),theme]);
+        }
+    console.log ('studentsCouplThemes',studentsCouplThemes)
+    return studentsCouplThemes;
+    
+}
+
+const studentsThemes = getStudentThemes (studentCoupl,themes);
+
+
+
+
+function markStudents (students,marks){
+    const markStedents = [];
+    for (let i = 0 ; i < students.length;i++){
       markStedents.push([students[i],marks[i]]);
   }
     console.log(markStedents);
-    
-    return studentCoupl.join('</br> ');
-}
+    return markStedents;
 
-function randomMark(){
-    let randomMark = Math.ceil((Math.random()* (5-1))+1);
+}
+const MarksStudents = markStudents (students,marks);
+
+
+function randomMark(maxmark,minmark){
+    const newMark = Math.ceil((Math.random()*(maxmark-minmark))+1);
     return randomMark;
 }
+const markForThemes = randomMark(maxmark,minmark);
 
 
+function newMarks (studentsThemes,markForThemes){
+    const newMarks = []
+    for(let i=0; i<studentsThemes.length;i++){
+        const themes1 = studentsThemes[i];
+        
+        console.log('themes1',themes1);
+      newMarks.push(themes1.join(', '),markForThemes);
+    }
+    console.log('newMarks',newMarks);
+    return newMarks;
+}
+const newRandomMark = newMarks(studentsThemes,maxmark,minmark)
 
-printStudents (students,themes,marks);
 
-document.writeln('',printStudents (students,themes,marks));
+document.writeln('</br>',studentCoupl,'</br>',studentsThemes,'</br>', newRandomMark);
 
 
