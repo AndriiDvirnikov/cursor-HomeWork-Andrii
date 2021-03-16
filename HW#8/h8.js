8a0e0c006fed62916e68892af686f96684144929
AndriiDvornikov <rostvorgame@gmail.com>
2021-03-16 12:58:02 +0200

    HW#8 all tasks

--- HW#8/h8.js
+++ HW#8/h8.js
@@ -1,5 +1,5 @@
 /*# У стдентів повинні бути наступні властивості: university, course, fullName, вони передаються при створенні студента(в конструктор).  */
-
+const midAvarage = 4;
 class Students {
     constructor (university,course,fullName, mark){
         this.university = university;
@@ -32,20 +32,28 @@ class Students {
         return avarege = (Math.round((avarege / this.marks.length)*10)/10);
     }
     dismiss(){
-      delete this.university;
-      delete this.course;
-      delete this.fullName;
-      this.marks = null;
+        delete this.university;
+        delete this.course;
+        delete this.fullName;
+        this.marks = null;
     }
 
-  recover() {
-    this.marks = [];
-  }
-   
+    recover() {
+        if (this.marks === null){
+            this.university = lastStudent.university;
+            this.course = lastStudent.course;
+            this.fullName = lastStudent.fullName;
+            this.marks = lastStudent.marks
+        }
+        
+    }
+       
 }
-
+let lastStudent = {};
 const firstStudent = new Students ('Політехніка', 3, 'Остап Бендер', [5,4,3,2,1,4]);
-console.log (firstStudent);
+Object.assign (lastStudent, firstStudent);
+
+
 
 /*#2 Створіть метод this.getInfo() -> "Студент 1го курсу Вищої Школи Психотерапії м.Одеса, Остап Родоманський Бендер", метод повертає сукупну інформацію про курс, учбовий заклад та імені студента.  */
 
@@ -61,14 +69,36 @@ console.log ('marks', firstStudent.getMarks);
 /*#5 Створіть метод отримання середнього балу this.getAverageMark() -> 4.6*/
 console.log('avereg',firstStudent.getAverageMark());
 /*#6 Створіть метод this.dismiss, який "виключить" студента. Після виклику цього методу – ставити студенту оцінки та отримувати їх більше не можна. (Ніяких помилок, просто повертається завжди null замість масиву оцінок) */
-firstStudent.dismiss();
-
-firstStudent.setMark = 3
-console.log ('marks', firstStudent.getMarks);
-
-
-console.log ('recover',firstStudent.recover())
-firstStudent.setMark = 3
-console.log ('marks', firstStudent.getMarks);
+ const dismiss = firstStudent.dismiss();
+console.log ('dismiss marks', firstStudent.getMarks);
+console.log ('dismiss student',dismiss)
 /*#7 Створіть метод this.recover, який дозволить поновити студента  */
 /*console.log ('recover', firstStudent.recover());*/
+firstStudent.recover();
+console.log ('recover', firstStudent);
+firstStudent.setMark = 2;
+console.log ('new mark after recover', firstStudent.getMarks);
+
+
+/*Створіть новий клас BudgetStudent, який повністю наслідує клас Student*/
+class BudgetStudent extends Students{
+    constructor (university,course,fullName, mark){
+        super(university,course,fullName, mark);
+        setTimeout (()=>{
+            this.getScholarship
+        },5000);
+       
+    }
+get getScholarship(){
+        if (avarege < midAvarage || this.marks=== null){
+            return console.log ('Стипендії нема або Вас відраховано');
+        }
+        console.log ('Ви отримали 1400 грн. стипендії');
+    }
+}
+
+const advansedStudent = new BudgetStudent ('Франка', 2, 'Остап Мамалига', [3,3,3,3,3,3])
+console.log ('advanced',advansedStudent);
+const avarege = advansedStudent.getAverageMark();
+console.log ('avarege',avarege);
+/*advansedStudent.getScholarship*/
