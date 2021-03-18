810d4112adb4f3e624ec8ede6b5b138fd1ad191b
AndriiDvornikov <rostvorgame@gmail.com>
2021-03-18 10:14:41 +0200

    HW#10 first version

---
+++ HW#10/h10.js
@@ -0,0 +1,35 @@
+/*Зробіть 25 квадратів розміру 50х50 пікселів кожен, зафарбовані у випадковий колір. Квадрати мають розташовуватись по 5 в ряд.
+Щоб квадрати з'явились на сторінці, необхідно викликати функцію generateBlocks();*/
+
+function randomColor (){
+    let randomColor = Math.trunc(Math.random () * 1000);
+    
+    if (randomColor < 100){
+        randomColor =randomColor * 10;
+    }
+    return '#'+randomColor;
+}
+function generateBlocks(){
+    const block = document.getElementsByClassName('block');
+    [...block].forEach (item =>{
+        item.style.background = randomColor();
+        item.style.width = '50px';
+        item.style.height = '50px';
+        item.style.margin = '5px';
+        item.style.float ='left';
+        item.style.border ='1px solid #000';
+    })
+    const holdBlock = document.getElementsByClassName('hold-block');
+    [...holdBlock].forEach (item => {
+    item.style.width = '325px';
+})
+}
+generateBlocks();
+
+/*Advanced зробіть так, щоб квадрати змінювали колір раз на секунду.
+Щоб квадрати з'явились на сторінці та почали змінюватись, необхідно викликати функцію generateBlocksInterval(); Приклад виконаного ДЗ на відео  */
+function generateBlocksInterval(){
+  const interval = setInterval(generateBlocks,1000);
+    setTimeout(() => { clearInterval(interval)}, 7000);
+}
+generateBlocksInterval()
