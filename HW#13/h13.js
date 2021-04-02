26b5d525312de553e4edb09d1184473874140631
AndriiDvornikov <rostvorgame@gmail.com>
2021-04-02 10:21:28 +0300

    add css

--- HW#13/h13.js
+++ HW#13/h13.js
@@ -1,9 +1,15 @@
 
+console.log('document.height',document.body.clientHeight)
 
 const btnInfo = document.querySelector('#getInfo')
 btnInfo.addEventListener('click', function(){
-    const snd = new Audio("sound/marsh.mp3");
-    snd.play();
+    /*const snd = new Audio("sound/marsh.mp3");
+    snd.play();*/
+    removeElemets ('.person');
+    removeElemets ('.planets');
+    clearHihgt ('#plan');
+  const blockHight = document.querySelector('#block')
+  blockHight.style.height = '520px'
   let film = 'https://swapi.dev/api/films/2/';
   fetch(film)
     .then((response)=>{
@@ -20,22 +26,19 @@ btnInfo.addEventListener('click', function(){
                 return response.json();
           })
           .then((data2)=>{
-              /*console.log(data2)
-              console.log(arr[i])*/
-              
-              /*console.log ('name: ',data2.name,'/ birth day: ',data2.birth_year,'gender: ', data2.gender);
-              */
+             
               str = 'name: '+data2.name+' / birth day: '+data2.birth_year+' / gender: '+data2.gender
               
               const text = document.querySelector('#block');
               const newElem = document.createElement("div");
               const addImg = document.createElement("img");
+              const addParag = document.createElement("div");
               addImg.className = 'personImg'
               addImg.src = 'img/'+i+'.jpg'
               newElem.className = 'person'
-              newElem.innerHTML = str;
-              text.appendChild(newElem).appendChild(addImg);
-              
+              addParag.innerHTML = str;
+              text.appendChild(newElem).appendChild(addImg)
+              newElem.appendChild(addParag);
           })
 
         }
@@ -43,67 +46,6 @@ btnInfo.addEventListener('click', function(){
        })
   
    })
-
-/*const btnPlanets = document.querySelector('#planets')
-btnPlanets.addEventListener('click', function getEPlanets (){
-    fetch('https://swapi.dev/api/planets')
-    .then((response)=>{
-        return response.json();
-    })
-    .then((planets)=>{
-        const planetsInfo = planets.results;
-        console.log (planets);
-      
-        for (let i =0; i<planetsInfo.length; i++){
-            const text = document.querySelector('#plan');
-            const newElem = document.createElement("div");
-            newElem.className = 'planets';
-            newElem.innerHTML = planetsInfo[i].name;
-            text.appendChild(newElem);
-            
-            console.log (planetsInfo[i].name);
-        }
-        const nextPage = planets.next
-        return nextPage
-    })
-    .then((nextPage)=>{
-            const nextPlanets = document.querySelector('#next');
-            nextPlanets.addEventListener ('click',()=>{
-                console.log (nextPage)
-                    /*nextP (nextPage)*/
-                   
-                    /*fetch(nextPage)
-                    .then((response)=>{
-                        return response.json();
-                    })
-                    .then((next)=>{
-                        nextP (next.next)
-                        const num = document.querySelectorAll('.planets')
-                        num.forEach(element => element.innerHTML='');
-                        const nextPlanets = next.results;
-                        const arr = [...num];
-
-                        for (let i=0; i< nextPlanets.length; i++){
-                            console.log (nextPlanets[i].name);
-                            num[i].innerHTML = nextPlanets[i].name;
-                        }
-
-
-                    })
-                   
-
-            })
-        
-    })
-    
-})*/
-let numPage = 1;
-const max = 6
-const btnPlanets = document.querySelector('#planets')
-btnPlanets.addEventListener('click', getPlanets)
-function getPlanets(){
-    getPagePlanets(numPage)
-}
 
  function getPagePlanets(numPage){
   
@@ -116,20 +58,34 @@ function getPlanets(){
          .then((planets)=>{
             const planetsInfo = planets.results;
             console.log (planets);
-      
+            const text = document.querySelector('#plan');
             for (let i =0; i<planetsInfo.length; i++){
-                const text = document.querySelector('#plan');
+            
                 const newElem = document.createElement("div");
                 newElem.className = 'planets';
+                text.style.height = '520px'
                 newElem.innerHTML = planetsInfo[i].name;
                 text.appendChild(newElem);
                 console.log (planetsInfo[i].name);
         }
+         zommBlock(text);
          
          })
      
      
     }
+
+let numPage = 1;
+const max = 6
+const btnPlanets = document.querySelector('#planets')
+btnPlanets.addEventListener('click', getPlanets)
+function getPlanets(){
+    removeElemets ('.person');
+    removeElemets ('.planets');
+    clearHihgt ('#block');
+    getPagePlanets(numPage)
+    
+}
 
 const nextPlanets = document.querySelector('#next');
 nextPlanets.addEventListener ('click',function(){
@@ -144,6 +100,7 @@ nextPlanets.addEventListener ('click',function(){
 
 const infoInput = document.querySelector ('#getFilm');
 infoInput.addEventListener ('click', function(){
+     removeElemets ('.film');
    const filmPage = document.getElementById("myinput").value;
    getFilmById (filmPage);
 })
@@ -160,14 +117,24 @@ function getFilmById (id){
         newElem.className = 'film';
         newElem.innerHTML = 'film #: '+id+' / title: '+film.title + ' / release  / date: ' + film.release_date + ' / producer: '+ film.producer;
         filmByValue.appendChild (newElem);
-         
         })
+   
 }
 
-fetch('https://swapi.dev/api/planets/1/?format=wookiee')
-    .then((response)=>{
-        return response.json();
-    })
-    .then((data)=>{
-        console.log (data);
-    })
+
+function removeElemets (elements){
+    const element = document.querySelectorAll(elements)
+    element.forEach (elem=> elem.remove ());
+}
+function clearHihgt (element){
+    const clearHight = document.querySelector (element);
+    clearHight.style.height = 0;
+}
+
+
+function zommBlock (element){
+      element.style.transform = 'scale(2)';
+}
+
+
+
